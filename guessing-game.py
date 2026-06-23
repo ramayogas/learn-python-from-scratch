24a1305a5e5d9bfc20fb4230bf49b3ac7c73f753
ramayogas <ramayogas.w@gmail.com>
2026-06-23 15:45:30 +0800

    done guessing game, on to the next one

--- guessing-game.py
+++ guessing-game.py
@@ -1,30 +1,24 @@
-import random
-# print(random.randrange(1, 11))
-# print(random.randint(1,11))
-
-def play_again():
-        restart = input("play again? (y/n): ")
-        if restart =="y":
-            game()
-        else:
-            print("Thanks for playing")
-
 def game():
     print("guess a number between 1 and 5")
     ran=random.randint(1,5)
-    while True:
-        try:
-            user_input = int(input())
-            game()
-        except ValueError:
-            print("bukan int")
+    
+    try:
+        user_input=int(input("enter your number: "))
+    except ValueError: 
+        print('not a number')
+        return
 
     if user_input == ran:
-        print("you win") 
+            print("you win") 
     else:    
         print("you lose")   
-        print("your number is", user_input, "and the number is", rangit)
-    play_again()
-    
+        print("your number is", user_input, "and the number is", ran) 
 game()
+while True:
+    restart = input("play again? (y/n): ")
     
+    if restart == "y":
+        game()
+    else:
+        print("thank you")
+        break
--- test.py
+++ test.py
@@ -1,6 +1,33 @@
-input = input("test input: ")
-if type(input) != int:
-    print("bukan int")
-print(type(input))
-print(input)
-tes
+import random
+
+# def play_again():
+#         restart = input("play again? (y/n): ")
+#         if restart =="y":
+#             game()
+#         else:
+#             print("Thanks for playing")
+
+def game():
+    print("guess a number between 1 and 5")
+    ran=random.randint(1,5)
+    
+    try:
+        user_input=int(input("enter your number: "))
+    except ValueError: 
+        print('not a number')
+        return
+
+    if user_input == ran:
+            print("you win") 
+    else:    
+        print("you lose")   
+        print("your number is", user_input, "and the number is", ran) 
+game()
+while True:
+    restart = input("play again? (y/n): ")
+    
+    if restart == "y":
+        game()
+    else:
+        print("thank you")
+        break
